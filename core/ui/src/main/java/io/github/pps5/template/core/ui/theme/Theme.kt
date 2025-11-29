4bd8c0e960b0e1ea1a1bde17952a93fd9fc6fc10
Naoki Nakajima <n2.jima@gmail.com>
2025-11-29 15:44:51 +0900

    Add theme previews

--- core/ui/src/main/java/io/github/pps5/template/core/ui/theme/Theme.kt
+++ core/ui/src/main/java/io/github/pps5/template/core/ui/theme/Theme.kt
@@ -55,3 +55,84 @@ fun {{project_name}}Theme(
         content = content
     )
 }
+
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun ThemeColorPreview() {
+    @Composable
+    fun ColorItem(color: Color, text: String) {
+        Surface(color = color, contentColor = contentColorFor(color)) {
+            Text(text = text, style = MaterialTheme.typography.h6)
+        }
+    }
+
+    {{project_name}}Theme {
+        Surface {
+            Column {
+                ColorItem(MaterialTheme.colors.primary, "primary")
+                ColorItem(MaterialTheme.colors.primaryVariant, "primaryVariant")
+                ColorItem(MaterialTheme.colors.secondary, "secondary")
+                ColorItem(MaterialTheme.colors.secondaryVariant, "secondaryVariant")
+                ColorItem(MaterialTheme.colors.background, "background")
+                ColorItem(MaterialTheme.colors.surface, "surface")
+                ColorItem(MaterialTheme.colors.error, "error")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ThemeTypographyPreview() {
+    {{project_name}}Theme {
+        Surface {
+            Column {
+                Text("H1 Title", style = MaterialTheme.typography.h1)
+                Text("H2 Title", style = MaterialTheme.typography.h2)
+                Text("H3 Title", style = MaterialTheme.typography.h3)
+                Text("H4 Title", style = MaterialTheme.typography.h4)
+                Text("H5 Title", style = MaterialTheme.typography.h5)
+                Text("H6 Title", style = MaterialTheme.typography.h6)
+                Text("Subtitle 1", style = MaterialTheme.typography.subtitle1)
+                Text("Subtitle 2", style = MaterialTheme.typography.subtitle2)
+                Text("Body 1", style = MaterialTheme.typography.body1)
+                Text("Body 2", style = MaterialTheme.typography.body2)
+                Text("Button", style = MaterialTheme.typography.button)
+                Text("Caption", style = MaterialTheme.typography.caption)
+                Text("Overline", style = MaterialTheme.typography.overline)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ThemeShapePreview() {
+    @Composable
+    fun ShapeItem(shape: Shape, name: String) {
+        Box(
+            modifier = Modifier
+                .clip(shape)
+                .background(MaterialTheme.colors.primary)
+                .size(100.dp),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(text = name, color = MaterialTheme.colors.onPrimary)
+        }
+    }
+
+    {{project_name}}Theme {
+        Surface {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                ShapeItem(MaterialTheme.shapes.small, "small")
+                ShapeItem(MaterialTheme.shapes.medium, "medium")
+                ShapeItem(MaterialTheme.shapes.large, "large")
+            }
+        }
+    }
+}
